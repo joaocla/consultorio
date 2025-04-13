f4a949884bbd1436c9b3194227f1db1280af9ca3
João Claudio de Andrade Bento <74563015+joaocla@users.noreply.github.com>
2025-04-13 14:35:05 -0300

    feat: cria e implementa os repositorios de Paciente, Medico e Consulta

---
+++ src/main/java/com/koda/consultorio/repository/ConsultaRepository.java
@@ -0,0 +1,8 @@
+package com.koda.consultorio.repository;
+
+import com.koda.consultorio.model.Consulta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+
+}
---
+++ src/main/java/com/koda/consultorio/repository/MedicoRepository.java
@@ -0,0 +1,8 @@
+package com.koda.consultorio.repository;
+
+import com.koda.consultorio.model.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+}
