4e8b096d88df9adb3cfba0b6f2f29e330ffbd046
João Claudio de Andrade Bento <74563015+joaocla@users.noreply.github.com>
2025-04-13 14:45:50 -0300

    feat: cria e implementa o Controller de paciente

---
+++ src/main/java/com/koda/consultorio/controller/PacienteController.java
@@ -0,0 +1,40 @@
+package com.koda.consultorio.controller;
+
+import com.koda.consultorio.model.Paciente;
+import com.koda.consultorio.repository.PacienteRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/pacientes")
+public class PacienteController {
+
+  private final PacienteRepository repository;
+
+  public PacienteController(PacienteRepository repository) {
+    this.repository = repository;
+  }
+
+  @GetMapping
+  public List<Paciente> listar(){
+    return repository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public Paciente buscar(@PathVariable Long id){
+    return repository.findById(id).orElse(null);
+  }
+
+  @PutMapping("/{id}")
+  public Paciente atualizar(@PathVariable Long id, @RequestBody Paciente paciente) {
+    paciente.setId(id);
+    return repository.save(paciente);
+  }
+
+  @DeleteMapping("/{id}")
+  public void deletar(@PathVariable Long id){
+    repository.deleteById(id);
+  }
+
+}
