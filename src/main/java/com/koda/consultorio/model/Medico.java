b757d06a6a11773fb93a50efdcd77295b1b7e4ff
João Claudio de Andrade Bento <74563015+joaocla@users.noreply.github.com>
2025-04-13 14:20:54 -0300

    feat: implementa entidade Medico

---
+++ src/main/java/com/koda/consultorio/model/Medico.java
@@ -0,0 +1,46 @@
+package com.koda.consultorio.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+public class Medico {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nome;
+  private String especialidade;
+  private String crm;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getEspecialidade() {
+    return especialidade;
+  }
+
+  public void setEspecialidade(String especialidade) {
+    this.especialidade = especialidade;
+  }
+
+  public String getCrm() {
+    return crm;
+  }
+
+  public void setCrm(String crm) {
+    this.crm = crm;
+  }
+}
