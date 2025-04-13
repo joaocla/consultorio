304a132a757f12ee1bce0d0041a81c5069109bb7
João Claudio de Andrade Bento <74563015+joaocla@users.noreply.github.com>
2025-04-13 14:18:10 -0300

    feat: inicia projeto implementando a entidade Paciente

---
+++ src/main/java/com/koda/consultorio/model/Paciente.java
@@ -0,0 +1,46 @@
+package com.koda.consultorio.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Paciente {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String nome;
+  private String cpf;
+  private String telefone;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getTelefone() {
+    return telefone;
+  }
+
+  public void setTelefone(String telefone) {
+    this.telefone = telefone;
+  }
+}
---
+++ src/test/java/com/koda/consultorio/ConsultorioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koda.consultorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultorioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
